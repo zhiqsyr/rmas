a1774a4c82dfa6bf11fcc6efbf427dacb49f1ae4
zhiqsyr <zhiqsyr@163.com>
2015-12-31 09:34:56 +0800

    DO 新增清除功能

--- source/rmas/src/java/main/com/dl/rmas/web/vm/produce/DoEditVM.java
+++ source/rmas/src/java/main/com/dl/rmas/web/vm/produce/DoEditVM.java
@@ -48,8 +48,9 @@ public class DoEditVM extends BaseVM {
 	 *
 	 */
 	@Command
-	@NotifyChange({"result"})
+	@NotifyChange({"result", "snDto"})
 	public void onAdd() {
+		// 校验：1）不能不输数据；2）客户属于同一家
 		if (StringUtils.isBlank(snDto.getRma())
 				&& snDto.getSnIndex() == null
 				&& StringUtils.isBlank(snDto.getSn())) {
@@ -57,7 +58,6 @@ public class DoEditVM extends BaseVM {
 			return;
 		}
 		
-		// 校验： 1）客户属于同一家
 		List<Sn> sns = snService.querySnsBySnDto(snDto, null);
 		if (sns.size() == 0) {
 			showInformationBox("There's no SN fit");
@@ -65,35 +65,58 @@ public class DoEditVM extends BaseVM {
 		}
 		
 		Sn vo = sns.get(0);
-		if (customerId == null) {								// 首次设置客户ID
+		if (result.size() == 0) {		// 结果集为空，重设客户ID
+			customerId = null;
+		}
+		if (customerId == null) {		// 设置客户ID
 			customerId = vo.getOrder().getCustomerId();
 		}
 
-		// 客户是否属于同一家
-		for (Sn sn : sns) {
+		for (Sn sn : sns) {		// 客户是否属于同一家
 			if (customerId != sn.getOrder().getCustomerId()) {	
 				showWarningBox("There is SN of other custom");
 				return;
 			}
 		}
 		
+		if (sns.size() > 1) {
+			if (!showQuestionBox("There are " + sns.size() + " SNs fit for you. Confirm to add all?")) {
+				return;
+			}
+		}
+		
 		// 新加 SN 放在 result 前面
 		List<Sn> added = new ArrayList<Sn>(result.size() + sns.size());	
+		StringBuilder contains = new StringBuilder();
 		for (Sn sn : sns) {
 			if (result.contains(sn)) {		// 已经加过，不再添加
+				contains.append(sn.getSn() + ", ");
 				continue;
 			}
 			added.add(sn);
 		}
+		if (StringUtils.isNotBlank(contains.toString())) {
+			String existings = contains.substring(0, contains.length() - 2).toString();
+			showInformationBox("These SNs " + existings + " are existing.");
+		}
 		
 		added.addAll(result);
 		result = added;
+		
+		// 清空输入
+		snDto.setRma(null);
+		snDto.setSnIndex(null);
+		snDto.setSn(null);
 	}
 	
 	@Command
-	@NotifyChange("snDto")
+	@NotifyChange("result")
 	public void onClear() {
-		snDto.setSn(null);
+		if (!showQuestionBox("Confirm to clear all below?")) {
+			return;
+		}
+		
+		result = new ArrayList<Sn>();
 	}
 	
 	@Command
